ed3e5708a0dc5383b9c67d2c76b90869c0283cde
Bruce Harvey <bruceharvey2000@gmail.com>
2020-04-06 14:21:03 -0400

    animation methods added
    pressing button in game makes a tap note fall from the sky, then deletes it

--- AlgoRhythm-master/app/src/main/java/com/example/algorhythm/Game.java
+++ AlgoRhythm-master/app/src/main/java/com/example/algorhythm/Game.java
@@ -1,5 +1,6 @@
 package com.example.algorhythm;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -8,18 +9,27 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator;
 
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -79,6 +89,8 @@ public class Game extends AppCompatActivity {
                         v, "translationY", v.getTranslationY() + 250);
                 animation.setDuration(2000);
                 animation.start();
+
+                createNote("tap", 2);
 
             }
         });
@@ -142,4 +154,90 @@ public class Game extends AppCompatActivity {
         return true;
     }
 
+    public void createNote(String type, final int noteNumber){
+        ImageView iv = new ImageView(getApplicationContext());
+
+        ConstraintLayout parentLayout = (ConstraintLayout)findViewById(R.id.ConstraintLayout);
+
+
+        //ImageView childView = new ImageView(this);
+        // set view id, else getId() returns -1
+
+
+        switch(type){
+            case "left":
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.noteleft));
+                break;
+            case "right":
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.noteright));
+                break;
+            case "tap":
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.notetap));
+                break;
+            default:
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.notetap));
+                break;
+        }
+
+        ConstraintSet set = new ConstraintSet();
+
+        //iv.setId(View.generateViewId());// set to current note number?
+        iv.setId(noteNumber);
+        //ConstraintLayout current = (ConstraintLayout)findViewById(R.id.constraintLayout);
+        parentLayout.addView(iv, 0);
+        set.clone(parentLayout);
+        // connect start and end point of views, in this case top of child to top of parent.
+        set.connect(iv.getId(), ConstraintSet.TOP, parentLayout.getId(), ConstraintSet.TOP, 60);
+        set.connect(iv.getId(), ConstraintSet.LEFT, parentLayout.getId(), ConstraintSet.LEFT, 60);
+        set.connect(iv.getId(), ConstraintSet.RIGHT, parentLayout.getId(), ConstraintSet.RIGHT, 60);
+
+
+        // ... similarly add other constraints
+        set.applyTo(parentLayout);
+
+        //RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+        //        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        //final TextView start = (TextView) findViewById(R.id.StartPoint);
+        //lp.addRule(RelativeLayout.BELOW, start.getId());
+        //iv.setLayoutParams(lp);
+        //final RelativeLayout r1 = (RelativeLayout) findViewById(R.id.ConstraintLayout);
+        //r1.addView(iv);
+        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationY", iv.getTranslationY() + 1400);
+        animation.setDuration(2000);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                removeNote(noteNumber);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+
+
+        });
+        animation.start();
+
+    }
+
+    public void removeNote(int noteNumber){
+        ImageView note = (ImageView)findViewById(noteNumber);
+        note.setVisibility(View.GONE);
+        ConstraintLayout parentLayout = (ConstraintLayout)findViewById(R.id.ConstraintLayout);
+        parentLayout.removeView(note);
+        //removeView(note);
+    }
 }
